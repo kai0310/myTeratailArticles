a65e7f0ed74bc88b5064ebb9130f7f3a93b548f6
motuo1201 <Enomoto Yousuke@DSP173N>
2017-09-21 18:05:13 +0900

    検索用のメソッドの引数をstring型にすることで、将来的な汎用性を持たせる

--- app/Services/utils.php
+++ app/Services/utils.php
@@ -10,19 +10,19 @@ class utils{
      */
     public function searchWord(Array $replies,Array $searchWords) {
         $filter = function($replies) use ($searchWords){
-            return $this->wordMatching($replies, $searchWords);
+            return $this->wordMatching($replies['body'], $searchWords);
         };
         return array_filter($replies, $filter);
     }
     /**
      * 配列内から全角・半角・大文字・小文字を無視して一致する内容を返却する。
-     * @param Array $searchObject 検索対象
+     * @param string $searchObject 検索対象
      * @param Array $searchWords 探したい文字配列
      * @return Array
      */
-    private function wordMatching(Array $searchObject,Array $searchWords){
+    private function wordMatching(string $searchObject,Array $searchWords){
         $stringMatching = function($searchValue) use ($searchObject){
-            if(mb_stripos(mb_convert_kana($searchObject['body']),mb_convert_kana($searchValue))!==false){
+            if(mb_stripos(mb_convert_kana($searchObject),mb_convert_kana($searchValue))!==false){
                 return true;
             }            
             return false;
