b1d4f9243ace8bb0f0b91c31929dd18a96d0a493
motuo1201 <Enomoto Yousuke@DSP173N>
2017-09-21 17:43:50 +0900

    foreachで処理をしていたが、array_filterを使う事で、可読性を向上

--- app/Services/utils.php
+++ app/Services/utils.php
@@ -5,25 +5,28 @@ class utils{
     /**
      * 配列内から検索ワードに該当する質問IDを配列で返却
      * @param array $replies
-     * @param String $searchWord
+     * @param Array $searchWords
      * @return array
      */
     public function searchWord(Array $replies,Array $searchWords) {
-        $articles = [];
-        $isPush = false;
-        foreach ($replies as $value) {
-            foreach ($searchWords as $searchValue) {
-                if(mb_stripos(mb_convert_kana($value['body']),mb_convert_kana($searchValue))){
-                    $isPush=true;
-                } else {
-                    $isPush=false;
-                }
-            }
-            if($isPush){
-                array_push($articles, $value);
-            }
-        }
-        return $articles;
+        $filter = function($replies) use ($searchWords){
+            return $this->wordMatching($replies, $searchWords);
+        };
+        return array_filter($replies, $filter);
+    }
+    /**
+     * 配列内から全角・半角・大文字・小文字を無視して一致する内容を返却する。
+     * @param Array $searchObject 検索対象
+     * @param Array $searchWords 探したい文字配列
+     * @return Array
+     */
+    private function wordMatching(Array $searchObject,Array $searchWords){
+        $stringMatching = function($searchValue) use ($searchObject){
+            if(mb_stripos(mb_convert_kana($searchObject['body']),mb_convert_kana($searchValue))!==false){
+                return true;
+            }            
+            return false;
+        };
+        return array_filter($searchWords, $stringMatching);
     }
 }
-
