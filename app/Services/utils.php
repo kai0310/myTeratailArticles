957fe81be3ca572b2619554b10990b89b255b8f1
motuo1201 <Enomoto Yousuke@DSP173N>
2017-09-22 11:35:45 +0900

    array_filterを使う事で、メソッド全体を見直し。可読性を向上

--- app/Services/utils.php
+++ app/Services/utils.php
@@ -3,30 +3,21 @@ namespace App\Services;
 
 class utils{
     /**
-     * 配列内から検索ワードに該当する質問IDを配列で返却
-     * @param array $replies
-     * @param Array $searchWords
-     * @return array
-     */
-    public function searchWord(Array $replies,Array $searchWords) {
-        $filter = function($replies) use ($searchWords){
-            return $this->wordMatching($replies['body'], $searchWords);
-        };
-        return array_filter($replies, $filter);
-    }
-    /**
-     * 配列内から全角・半角・大文字・小文字を無視して一致する内容を返却する。
-     * @param string $searchObject 検索対象
-     * @param Array $searchWords 探したい文字配列
+     * 配列内から検索ワードに該当する質問IDを配列で返却(AND条件)
+     * 全角・半角、大文字・小文字の違いは無視する。
+     * @param Array $articles 検索対象の記事
+     * @param Array $searchWords 検索文言
      * @return Array
      */
-    private function wordMatching(string $searchObject,Array $searchWords){
-        $stringMatching = function($searchValue) use ($searchObject){
-            if(mb_stripos(mb_convert_kana($searchObject),mb_convert_kana($searchValue))!==false){
-                return true;
-            }            
-            return false;
+    public function searchWord(Array $articles,Array $searchWords) {
+        $filter = function($searchObject) use ($searchWords){
+            foreach ($searchWords as $searchWord) {
+                if(mb_stripos(mb_convert_kana($searchObject['body']),mb_convert_kana($searchWord))===false){
+                    return false;
+                }
+            }
+            return true;
         };
-        return array_filter($searchWords, $stringMatching);
+        return array_filter($articles, $filter);
     }
 }
